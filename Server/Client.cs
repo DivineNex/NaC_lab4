a5bd2d09aad7f0b19b737f68a971a22c7bd67b79
Nikolay Makalov <nexterrible@mail.ru>
2022-04-10 13:58:08 +0700

    Multi-client and client disconnect!

---
+++ Server/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal struct Client
+    {
+        public string ip_port;
+        public eClientType type;
+        public Thread socketThread;
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -10,7 +10,7 @@ namespace Server
     class Program
     {
         private static Thread listeningThread;
-        private static List<Thread> clientThreads;
+        private static List<Client> clients;
         private static IPEndPoint ipPoint;
         private static Socket listenSocket;
         static int port = 55555;
@@ -25,7 +25,7 @@ namespace Server
         {
             ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
             listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            clientThreads = new List<Thread>();
+            clients = new List<Client>();
         }
 
         private static void Start()
@@ -33,26 +33,6 @@ namespace Server
             listeningThread = new Thread(() => ListeningThread());
             listeningThread.Start();
             Console.ReadKey();
-        }
-
-        private static void SocketThread(Socket handler)
-        {
-            while (true)
-            {
-                StringBuilder builder = new StringBuilder();
-                int bytes = 0;
-                byte[] data = new byte[256];
-
-                do
-                {
-                    bytes = handler.Receive(data);
-                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                }
-                while (handler.Available > 0);
-
-                string recievedMessage = builder.ToString();
-                Console.WriteLine(recievedMessage);
-            }
         }
 
         private static void ListeningThread()
@@ -72,15 +52,81 @@ namespace Server
                     Console.WriteLine($"Клиент {handler.RemoteEndPoint} подключен");
                     Console.BackgroundColor = ConsoleColor.Black;
 
-                    Thread newThread = new Thread(() => SocketThread(handler));
+                    Client newClient = new Client();
+                    newClient.ip_port = handler.RemoteEndPoint.ToString();
+
+                    Thread newThread = new Thread(() => SocketThread(handler, newClient));
                     newThread.Start();
-                    clientThreads.Add(newThread);
+
+                    newClient.socketThread = newThread;
+
+                    //ТРЕБУЕТСЯ ПОЛНАЯ СИСТЕМА ТИПИЗАЦИИ КЛИЕНТОВ
+                    //newClient.type = eClientType.Reg_module;
+
+                    clients.Add(newClient);
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
             }
+        }
+
+        private static void SocketThread(Socket handler, Client client)
+        {
+            while (true)
+            {
+                try
+                {
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+
+                        if (bytes <= 0)
+                        {
+                            throw new SocketException();
+                        }
+
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+                    string recievedMessage = builder.ToString();
+                    Console.WriteLine(recievedMessage);
+                }
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"Клиент {client.ip_port} отключен");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    handler.Close();
+                    clients.Remove(client);
+                    break;
+                }
+            }
+
+            //Старая система получения сообщений. Пока пусть тут будет
+
+            //while (client.connected)
+            //{
+            //    StringBuilder builder = new StringBuilder();
+            //    int bytes = 0;
+            //    byte[] data = new byte[256];
+
+            //    do
+            //    {
+            //        bytes = handler.Receive(data);
+            //        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            //    }
+            //    while (handler.Available > 0);
+
+            //    string recievedMessage = builder.ToString();
+            //    Console.WriteLine(recievedMessage);
+            //}
         }
     }
 }
